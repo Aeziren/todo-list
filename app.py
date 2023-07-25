ac0375fa0beec4656545309c2eb57488eeb79364
Miguel Silva <123553708+Aeziren@users.noreply.github.com>
2023-07-25 17:08:25 +0000

    Implemantadas opções e a opção 4 (quit)

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ while True:
 3. Remove Task
 4. Quit""")
     op = int(input("Option: "))
+
+    match op:
+        case 1:
+            print("Você inseriu 1.")
+        case 2:
+            print("Você inseriu 2.")
+        case 3:
+            print("Você inseriu 3.")
+        case 4:
+            print("Bye! =D")
+            break
+        case _:
+            print("Invalid Option!\n")
