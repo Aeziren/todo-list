4d4d6f952abb504b12bdc1f4b93ff8695c778411
Miguel Silva <123553708+Aeziren@users.noreply.github.com>
2023-07-27 09:39:38 +0000

    Function "add tasks" now working.

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import sqlite3
 
-conection = sqlite3.connect("tasks.db")
-db = conection.cursor()
+def add_task():
+    while True:
+        task = input("Task Description: ").strip()
+        if task:
+            break
+
+    exp = input("Expiry date (optional): ").strip()
+
+    db.execute("INSERT INTO tasks (objective, expiry) VALUES (?, ?)", (task, exp))
+    connection.commit()
+    print(f"Added {task} to tasks!")
+
+
+connection = sqlite3.connect("tasks.db")
+db = connection.cursor()
 
 db.execute("SELECT count(name) FROM sqlite_master WHERE type = 'table' AND name = 'tasks'")
 
@@ -16,24 +29,25 @@ if not db.fetchone()[0]:
 
 print("=TO DO LIST=")
 while True:
-    print("""1. Add Task
-2. Mark task as done
-3. Remove Task
-4. View Tasks
+    print("""1. View Tasks
+2. Add task
+3. Mark task as done
+4. Remove task
 5. Quit""")
     op = int(input("Option: "))
 
     match op:
         case 1:
-            print("Você inseriu 1.")
+            print("Op1")
         case 2:
-            print("Você inseriu 2.")
+            add_task()
         case 3:
             print("Você inseriu 3.")
         case 4:
             print("Você inseriu 4.")
         case 5:
             print("Bye! =D")
+            connection.close()
             break
         case _:
             print("Invalid Option!\n")
